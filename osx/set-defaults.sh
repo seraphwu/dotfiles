84bc4a38eee1d43756299f498a87f27117898a1d
Seraph Wu <seraphwu@gmail.com>
2022-03-21 18:58:36 +0800

    Update set-defaults.sh

--- osx/set-defaults.sh
+++ osx/set-defaults.sh
@@ -52,7 +52,7 @@ sudo spctl --master-disable
 # done
 defaults write com.apple.systemuiserver menuExtras -array \
  	"/System/Library/CoreServices/Menu Extras/Bluetooth.menu" \
- 	"/System/Library/CoreServices/Menu Extras/AirPort.menu" \
+# 	"/System/Library/CoreServices/Menu Extras/AirPort.menu" \
  	"/System/Library/CoreServices/Menu Extras/Battery.menu" \
  	"/System/Library/CoreServices/Menu Extras/Clock.menu"
 
